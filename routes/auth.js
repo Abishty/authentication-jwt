31a171279498a2a7a20a1ad8b5e3793065a09a6b
ab-bisht <18abhishek.b@gmail.com>
2020-05-18 22:21:58 +0530

    email and password validation added

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/User');
+const { registerValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
+    const { error } = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message)
+    
     const user = new User({
         name: req.body.name,
         email: req.body.email,
@@ -13,8 +17,8 @@ router.post('/register', async (req, res) => {
     try {
         const savedUser = await user.save();
         res.send(savedUser);
-    } catch (error) {
-        res.status(400).send(error)
+    } catch (err) {
+        res.status(400).send(err)
     }
 })
 
