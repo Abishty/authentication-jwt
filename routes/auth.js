b98d65f265d8f012ee640a5129e4f4919a550807
ab-bisht <18abhishek.b@gmail.com>
2020-05-19 17:39:54 +0530

    duplicate email handled

--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,15 @@ const User = require('../models/User');
 const { registerValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
+    // validate the data before we make a user
     const { error } = registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message)
     
+    // checking if the user already in database
+    const emailExist = await User.findOne({email: req.body.email})
+    if(emailExist) return res.status(400).send('Email already exist')
+
+    // create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
